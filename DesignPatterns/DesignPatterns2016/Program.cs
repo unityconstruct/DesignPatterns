95c2b870246e907a45d275e16dd350cecc3b4a1f
silosix <silosix@W7-DEV1>
2018-07-04 13:47:43 -0500

    Created DesignPatters Solution and DesignPatterns2016 project

---
+++ DesignPatterns/DesignPatterns2016/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatterns2016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
