d1df0c76bac590caf6f84ce08af22a069b89a3fa
silosix <silosix@W7-DEV1>
2018-07-06 19:37:00 -0500

    Added FieldsAndProperties example for get/set Validation & LazyLoading

---
+++ DesignPatterns/DesignPatterns2016/FieldsAndProperties/UsingProperties.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns2016
+{
+    class UsingProperties
+    {
+        private string _name = "default";
+        private Thingy _thingy;
+
+        // set allows for validation logic(set)
+        public string Name
+        {
+            get { return _name; }
+            set {
+                var formattedName = value?.Trim();
+                _name = formattedName;
+            }
+        }
+
+        //get allows for lazy loading a 'Thingy' object using the getter
+        // used for Singleton Design Pattern
+        public Thingy AThingy
+        {
+            get
+            {
+                if (_thingy == null)
+                {
+                    _thingy = new Thingy();
+                }
+                return _thingy;
+            }
+            set { _thingy = value; }
+        }
+
+    }
+
+    class Thingy {
+        public string Name;
+        public string Value;
+    }
+
+}
